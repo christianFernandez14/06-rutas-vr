f125c37392a8e65444de6cecd3f98ce1625a0457
Christian Fernandez <christian14@gmail.com>
2024-02-14 09:37:43 -0300

    Enlaces SPA / la explicación de donde usar NavLink y Link / .97

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
     <div>
       <h1>Error</h1>
       <p>Pagina de Error</p>
+      <Link to={'/inicio'}>Volver al incio</Link>
     </div>
   )
 }
