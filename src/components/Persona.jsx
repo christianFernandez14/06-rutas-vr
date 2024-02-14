3160ffe6c5d718f6b962344816f5185631bfa468
Christian Fernandez <christian14@gmail.com>
2024-02-14 10:31:10 -0300

    Trabajando con el useNavigate, para redireccionar mi SPA / .101

--- src/components/Persona.jsx
+++ src/components/Persona.jsx
@@ -1,21 +1,37 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 
 const Persona = () => {
 
   const parametros = useParams()
   const { nombre, apellido } = parametros
-  
-  // Como otra opcion en los paremetros por default, se lo asignas en la destructuracion
-  // const { nombre='Usuario', apellido='Apellido del Usuario' } = parametros
-  
 
-  // console.log(parametros)
+  const navegar = useNavigate()
+
+  console.log(navegar)
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    let nombre = e.target.nombre.value
+    let apellido = e.target.apellido.value
+    const URL = `/persona/${nombre}/${apellido}`
+
+    navegar(URL)
+
+  }
 
   return (
     <div>
       <h1>Bienvenido; {!nombre ? 'Usuario' : nombre} {!apellido ? "" : apellido }</h1>
       <p>Esta es la pagina es de Persona</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="nombre" />
+        <input type="text" name="apellido" />
+        <input type="submit" name="enviar" value='Enviar' />
+      </form>
     </div>
   )
 }
